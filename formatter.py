287ef26c2c700358368b2571fb015205107c4244
Oscar Wu <wuy7117@gmail.com>
2023-08-19 05:26:58 +1000

    update with spelling check

--- formatter.py
+++ formatter.py
@@ -3,23 +3,31 @@ import re
 import argparse
 import urllib.parse
 import webbrowser
+from autocorrect import Speller
+
 class Converter:
-    
-    def __init__(self):
+    def __init__(self, spell = True):
         self.source_directory = 'raw_txt'
         self.destination_directory = 'fast_txt'
+        self.spell = spell
+
     
-    @staticmethod
-    def format_text(text):
+    def format_text(self, text):
         # Extract English words and punctuation
         items = re.findall(r'\b[a-zA-Z]+(?:-[a-zA-Z]+)*\'?[a-zA-Z]*\b|[.,!?;]', text)
         
-        # Remove duplicates but maintain order
-        ordered_unique_items = []
-        for item in items:
-            if item not in ordered_unique_items:
-                ordered_unique_items.append(item)
-        
+        if self.spell:
+            # Combine the items into a single string
+            combined_text = ' '.join(items)
+
+            # Spell correct the combined text
+            blob =  Speller()
+            corrected_text = blob(combined_text)
+
+            # Split the corrected text to get the items and format them
+            items = corrected_text.split()        
+
+
         # Join the items with '|'
         formatted_text = '|'.join(items)
         return formatted_text
@@ -27,8 +35,11 @@ class Converter:
     def convert_file(self, filename):
         input_filepath = os.path.join(self.source_directory, filename)
         
+        assert os.path.exists(input_filepath), f"Error: {input_filepath} does not exist!"
+        
         with open(input_filepath, 'r') as file:
             content = file.read()
+        assert type(content) == str, f"Error: {content[:10]} does not exist!"
         
         # Format the content
         result = self.format_text(content)
@@ -39,8 +50,9 @@ class Converter:
         
         # Write the result to the output file
         with open(output_filepath, 'w') as file:
-            file.write(result)
+            file.write(self.format_lines(result))
         return result 
+    
     def process_directory(self):
         # Check and create destination directory if it doesn't exist
         if not os.path.exists(self.destination_directory):
@@ -60,6 +72,28 @@ class Converter:
         }
         url = f"{base_url}?{urllib.parse.urlencode(params)}"
         return url
+    
+    def format_lines(self, text, max_line_length=87):
+        words = text.split('|')
+        lines = []
+        current_line = []
+
+        current_length = 0
+        for word in words:
+            if current_length + len(word) + 1 > max_line_length:  # +1 for the '|'
+                lines.append('|'.join(current_line))
+                current_length = 0
+                current_line = []
+
+            current_line.append(word)
+            current_length += len(word) + 1  # +1 for the '|'
+
+        # Append any remaining words to the lines
+        if current_line:
+            lines.append('|'.join(current_line))
+
+        return '\n'.join(lines)
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert English text with punctuation in files to specified format.')
     
